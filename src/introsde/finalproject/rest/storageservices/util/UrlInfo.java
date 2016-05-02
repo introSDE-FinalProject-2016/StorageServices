b3a849295d7d2c405f23b15fc1b8bdc0bf6bfd47
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-02 12:50:33 +0200

    set url about services module

--- src/introsde/finalproject/rest/storageservices/util/UrlInfo.java
+++ src/introsde/finalproject/rest/storageservices/util/UrlInfo.java
@@ -4,16 +4,20 @@ public class UrlInfo {
 	
 	public UrlInfo() {}
 	
-	static final String localDBurl = "https://nameless-reaches-22539.herokuapp.com/ws/people?wsdl";
-	static final String adapterUrl = "https://desolate-scrubland-21919.herokuapp.com/sdelab/person";
-	static final String storageUrl = "";
+	//static final String localDBurl = "https://nameless-reaches-22539.herokuapp.com/ws/people?wsdl";
+	static final String localDBUrl = "http://127.0.1.1:6902/ws/people?wsdl";
+	
+	//static final String adapterUrl = "https://desolate-scrubland-21919.herokuapp.com/sdelab/person";
+	static final String adapterUrl = "http://127.0.1.1:5702/sdelab/adapter-service";
+	
+	static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
 	
 	/**
 	 * This method is used to get the local database url
 	 * @return
 	 */
 	public String getLocalDBURL() {
-		return localDBurl;
+		return localDBUrl;
 	}
 	
 	/**
