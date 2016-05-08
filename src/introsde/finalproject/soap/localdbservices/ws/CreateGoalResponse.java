9793019913b9dcb414fd30e1cad6b2dac7d78d16
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-09 00:30:15 +0200

    update encoding

--- src/introsde/finalproject/soap/localdbservices/ws/CreateGoalResponse.java
+++ src/introsde/finalproject/soap/localdbservices/ws/CreateGoalResponse.java
@@ -34,7 +34,7 @@ public class CreateGoalResponse {
     protected int gid;
 
     /**
-     * Recupera il valore della proprietà gid.
+     * Recupera il valore della proprieta gid.
      * 
      */
     public int getGid() {
@@ -42,7 +42,7 @@ public class CreateGoalResponse {
     }
 
     /**
-     * Imposta il valore della proprietà gid.
+     * Imposta il valore della proprieta gid.
      * 
      */
     public void setGid(int value) {
