9c6381ed1ce6ada679c6ded33cefbf385efbb9c8
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-09 00:13:16 +0200

    update encoding

--- src/introsde/finalproject/soap/localdbservices/ws/GetPerson.java
+++ src/introsde/finalproject/soap/localdbservices/ws/GetPerson.java
@@ -34,7 +34,7 @@ public class GetPerson {
     protected int pid;
 
     /**
-     * Recupera il valore della proprietà pid.
+     * Recupera il valore della proprieta pid.
      * 
      */
     public int getPid() {
@@ -42,7 +42,7 @@ public class GetPerson {
     }
 
     /**
-     * Imposta il valore della proprietà pid.
+     * Imposta il valore della proprieta pid.
      * 
      */
     public void setPid(int value) {
