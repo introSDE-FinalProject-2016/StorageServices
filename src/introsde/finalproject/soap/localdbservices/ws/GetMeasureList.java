97bee6e99340d78154235a675b9fe1bb627f4e6b
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-09 00:02:06 +0200

    update encoding

--- src/introsde/finalproject/soap/localdbservices/ws/GetMeasureList.java
+++ src/introsde/finalproject/soap/localdbservices/ws/GetMeasureList.java
@@ -34,7 +34,7 @@ public class GetMeasureList {
     protected int pid;
 
     /**
-     * Recupera il valore della proprietà pid.
+     * Recupera il valore della proprieta pid.
      * 
      */
     public int getPid() {
@@ -42,7 +42,7 @@ public class GetMeasureList {
     }
 
     /**
-     * Imposta il valore della proprietà pid.
+     * Imposta il valore della proprieta pid.
      * 
      */
     public void setPid(int value) {
