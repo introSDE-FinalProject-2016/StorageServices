8350800b81757e8760cab6342d9d72ecaa396e56
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-06-02 12:08:10 +0200

    update url adapter service

--- src/introsde/finalproject/rest/storageservices/util/UrlInfo.java
+++ src/introsde/finalproject/rest/storageservices/util/UrlInfo.java
@@ -7,7 +7,7 @@ public class UrlInfo {
 	static final String localDBUrl = "https://nameless-reaches-22539.herokuapp.com/ws/people?wsdl";
 	//static final String localDBUrl = "http://127.0.1.1:6902/ws/people?wsdl";
 	
-	static final String adapterUrl = "https://desolate-scrubland-21919.herokuapp.com/sdelab/adapter-service";
+	static final String adapterUrl = "https://stark-island-39603.herokuapp.com/sdelab/adapter-service";
 	//static final String adapterUrl = "http://127.0.1.1:5702/sdelab/adapter-service";
 	
 	static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
